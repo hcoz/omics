cb45021222dcde31276cac7aa67246a1f725ef52
Halil Ozcelik <hcanozcelik@gmail.com>
2025-03-06 16:53:47 +0300

    Gene stats moved to modal

--- frontend/src/components/GeneSearch.js
+++ frontend/src/components/GeneSearch.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import StatsModal from './StatsModal';
 
 const GeneSearch = () => {
   const [geneInput, setGeneInput] = useState('');
@@ -96,33 +97,7 @@ const GeneSearch = () => {
         </div>
       )}
 
-      {statsData && (
-        <div className="stats">
-          <h3>Statistics for {statsData.geneId}</h3>
-          <div className="stats-tables">
-            <div>
-              <h4>Experimental Samples</h4>
-              <table>
-                <tbody>
-                  <tr><td>Mean</td><td>{statsData.experimental.mean}</td></tr>
-                  <tr><td>Median</td><td>{statsData.experimental.median}</td></tr>
-                  <tr><td>Variance</td><td>{statsData.experimental.variance}</td></tr>
-                </tbody>
-              </table>
-            </div>
-            <div>
-              <h4>Control Samples</h4>
-              <table>
-                <tbody>
-                  <tr><td>Mean</td><td>{statsData.control.mean}</td></tr>
-                  <tr><td>Median</td><td>{statsData.control.median}</td></tr>
-                  <tr><td>Variance</td><td>{statsData.control.variance}</td></tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      )}
+      <StatsModal statsData={statsData} onClose={() => setStatsData(null)} />
     </div>
   );
 };
---
+++ frontend/src/components/StatsModal.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const StatsModal = ({ statsData, onClose }) => {
+  if (!statsData) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h3>Statistics for {statsData.geneId}</h3>
+          <button className="close-button" onClick={onClose}>&times;</button>
+        </div>
+        <div className="stats-tables">
+          <div>
+            <h4>Experimental Samples</h4>
+            <table>
+              <tbody>
+                <tr><td>Mean</td><td>{statsData.experimental.mean}</td></tr>
+                <tr><td>Median</td><td>{statsData.experimental.median}</td></tr>
+                <tr><td>Variance</td><td>{statsData.experimental.variance}</td></tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <h4>Control Samples</h4>
+            <table>
+              <tbody>
+                <tr><td>Mean</td><td>{statsData.control.mean}</td></tr>
+                <tr><td>Median</td><td>{statsData.control.median}</td></tr>
+                <tr><td>Variance</td><td>{statsData.control.variance}</td></tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatsModal; 
